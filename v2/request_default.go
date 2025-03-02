86255eaaf78ff53b7612959b3509201e478d89b5
Kirill Scherba <kirill@scherba.ru>
2025-03-02 11:11:06 +0300

    The GetConnectionChannel added to default request

--- v2/request_default.go
+++ v2/request_default.go
@@ -1,6 +1,10 @@
 package command
 
-import "time"
+import (
+	"time"
+
+	"github.com/kirill-scherba/command/v2/subscription"
+)
 
 type DefaultRequest struct {
 	Vars map[string]string
@@ -13,6 +17,10 @@ func (r *DefaultRequest) GetVars() map[string]string {
 
 func (r *DefaultRequest) GetData() []byte {
 	return r.Data
+}
+
+func (r *DefaultRequest) GetConnectionChannel() subscription.ConnectionChannel {
+	return nil
 }
 
 func (r *DefaultRequest) SetDate(date time.Time) {
